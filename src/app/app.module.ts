504aa15c21e19808f3ea91cb3d1d5d3a1d7eae6a
Cristian Espiritu <cxistian@gmail.com>
2017-12-20 16:29:37 -0600

    Module Skeletons Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { FormBusquedaComponent } from './form-busqueda/form-busqueda.component';
+import { ViewFacturaComponent } from './view-factura/view-factura.component';
+import { FormLogInComponent } from './form-log-in/form-log-in.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormBusquedaComponent,
+    ViewFacturaComponent,
+    FormLogInComponent
   ],
   imports: [
     BrowserModule,
