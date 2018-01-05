9b92b9fa058480bcf2f4385bcdc2fc4600d3868d
Cristian Espiritu <cxistian@gmail.com>
2018-01-05 17:45:40 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static('./dist'))
 app.use('/*', express.static(path.resolve('dist/index.html')))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('dist/index.html'))
@@ -19,7 +24,7 @@ app.use(function (req, res, next) {
   err.status = 404
   next(err)
 })
-if (app.get('env== "development"')) {
+if (app.get('env') === 'development') {
   app.listen(3000, function () {
     console.log('Example listening on port 3000!')
   })
